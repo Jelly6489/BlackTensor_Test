d8e8b751c7a1dd3fa2a1369597378ff5b136e5e7
jelly6489 <rlaalsrlzld@naver.com>
2020-10-31 11:59:08 +0900

    Team BlackTensor_Test commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from hello import HelloWorld
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
